babf10fbea663cd6d1b254b26eec4250822bba94
jayaarun291087 <jaya26it@gmail.com>
2023-02-04 23:01:31 +0530

    done

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Oneplus 10R",
+    brand: "Oneplus",
+    desc: "6.7-inch display",
+    price: 349,
+    image:
+      "https://res.cloudinary.com/dvkw32jay/image/upload/v1672988889/onlineShop/OnePlus_10R_5G_8GB_olpsro.png",
+  },
+  {
+    id: 2,
+    name: "Galaxy S22",
+    brand: "Samsung",
+    desc: "6.4-inch mini display",
+    price: 110,
+    image:
+      "https://res.cloudinary.com/dvkw32jay/image/upload/v1672988898/onlineShop/Samsung_Galaxy_s22_ultra_bjj12d.png",
+  },
+  {
+    id: 3,
+    name: "iphone 12",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 600,
+    image:
+      "https://res.cloudinary.com/dvkw32jay/image/upload/v1672988873/onlineShop/apple_iphone_12_128_gb_fdkjlz.png",
+  },
+];
+
+module.exports = products;
---
+++ utils/genAuthToken.js
@@ -0,0 +1,17 @@
+const jwt= require("jsonwebtoken");
+const genAuthToken = (user)=> {
+  const jwtSecretKey=process.env.JWT_SECRET_KEY;
+  const token = jwt.sign({
+    _id: user._id,
+    name: user.name, 
+    email:user.email,
+    isAdmin:user.isAdmin,
+
+  },
+   jwtSecretKey
+  );
+  return token;
+
+};
+
+module.exports= genAuthToken;
